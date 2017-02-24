e3d8a735612fb687372f44d6ab704a59b1c3f1f4
Andy Pye <andrew.pye@silvercurve.co.uk>
2017-02-24 09:21:36 +0200

    Look up users from backend

--- app/login/route.js
+++ app/login/route.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-const { Route } = Ember;
+const { Route, get } = Ember;
 
 export default Route.extend(UnauthenticatedRouteMixin, {
+	model () {
+		return get(this, 'store').findAll('user');
+	}
 });
