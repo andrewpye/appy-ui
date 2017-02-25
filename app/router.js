8067661795dae45cb2f4025ad2426db4d47d45e6
Andy Pye <andrew.pye@silvercurve.co.uk>
2017-02-25 15:08:41 +0200

    Add apps route. Rejig some styling.

--- app/router.js
+++ app/router.js
@@ -7,7 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('authenticated', { path: '/' }, function() {});
+  this.route('authenticated', { path: '/' }, function() {
+    this.route('apps');
+  });
   this.route('login');
 });
 
