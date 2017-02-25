ac3a21dde85479f0b1f94b58540e2b7fd1db7c27
Andy Pye <andrew.pye@silvercurve.co.uk>
2017-02-25 14:54:43 +0200

    Change header name in call authorization

--- app/authorizers/user-model.js
+++ app/authorizers/user-model.js
@@ -3,6 +3,6 @@ import BaseAuthorizer from 'ember-simple-auth/authorizers/base';
 export default BaseAuthorizer.extend({
 	authorize (sessionData, block) {
 		// Send the user ID in a header so the backend knows who's making a request.
-		block('Authorization', sessionData.userId);
+		block('User-Id', sessionData.userId);
 	}
 });
