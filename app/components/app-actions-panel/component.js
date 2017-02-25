27d88d608d8b4eb4bd62f0c46d5c5b4d90a6020f
Andy Pye <andrew.pye@silvercurve.co.uk>
2017-02-25 22:00:44 +0200

    Allow publishing/approval of apps

--- app/components/app-actions-panel/component.js
+++ app/components/app-actions-panel/component.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
+import { task } from 'ember-concurrency';
 
-export default Ember.Component.extend({
+const { Component, get, set } = Ember;
+
+export default Component.extend({
+	setAppStatusTask: task(function* (status) {
+		const app = get(this, 'app');
+		set(app, 'status', status);
+		yield app.save();
+	})
 });
