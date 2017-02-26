fd222158cac167a26cf16f69c879d5d1435070bf
Andy Pye <andrew.pye@silvercurve.co.uk>
2017-02-26 10:04:24 +0200

    DDAU refactor on app actions panel. Improve styling.

--- app/authenticated/apps/route.js
+++ app/authenticated/apps/route.js
@@ -1,9 +1,22 @@
 import Ember from 'ember';
 
-const { Route, get } = Ember;
+const { Route, get, set } = Ember;
 
 export default Route.extend({
 	model () {
 		return get(this, 'store').findAll('app');
+	},
+
+	actions: {
+		onEditApp (app) {},
+
+		onDeleteApp (app) {
+			return app.destroyRecord();
+		},
+
+		onSetAppStatus (app, status) {
+			set(app, 'status', status);
+			return app.save();
+		}
 	}
 });
--- app/components/app-actions-panel/component.js
+++ app/components/app-actions-panel/component.js
@@ -1,16 +1,10 @@
 import Ember from 'ember';
-import { task } from 'ember-concurrency';
 
-const { Component, get, set } = Ember;
+const { Component } = Ember;
 
 export default Component.extend({
-	setAppStatusTask: task(function* (status) {
-		const app = get(this, 'app');
-		set(app, 'status', status);
-		yield app.save();
-	}),
-
-	deleteAppTask: task(function* () {
-		yield get(this, 'app').destroyRecord();
-	})
+	// Default handlers to be overridden by attributes.
+	onEditApp () {},
+	onDeleteApp () {},
+	onSetAppStatus () {}
 });
