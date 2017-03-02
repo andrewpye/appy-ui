13d8015acbefa1a09adde9709af4de4bd57633ff
Andy Pye <andrew.pye@silvercurve.co.uk>
2017-03-02 07:30:32 +0200

    Reinstate app info dropdown

--- app/components/app-info/component.js
+++ app/components/app-info/component.js
@@ -3,8 +3,5 @@ import Ember from 'ember';
 const { Component } = Ember;
 
 export default Component.extend({
-	classNames: [ 'app-info' ],
-
-	// Default handler to be overridden by attributes.
-	onClick () {}
+	classNames: [ 'app-info' ]
 });
