a70b3bb9a71f0d795651d76d43a59537ea558a10
Andy Pye <andrew.pye@silvercurve.co.uk>
2017-03-02 20:51:04 +0200

    Refactor app editor component to use file picker and handle loading local images

--- app/components/app-editor/component.js
+++ app/components/app-editor/component.js
@@ -1,16 +1,42 @@
 import Ember from 'ember';
 import appValidations from 'appy-ui/app/validations';
+import { task } from 'ember-concurrency';
 
-const { Component } = Ember;
+const { Component, inject, get, set } = Ember;
 
 export default Component.extend({
+	fileReader: inject.service(),
+
 	appValidations,
 
 	classNames: [ 'app-editor' ],
 
 	submitButtonText: 'Save',
 
-	// Default handlers to be overridden by attributes.
-	onSave () {},
-	onCancel () {}
+	updateImageFileTask: task(function* (imageFile) {
+		const dataUrl = imageFile ? yield get(this, 'fileReader').readBase64Data(imageFile) : null;
+		set(this, 'app.image.url', dataUrl);
+	}).restartable(),
+
+	actions: {
+		save (...args) {
+			// Wait for any local image loading to finish.
+			return get(this, 'updateImageFileTask.last')
+			.then(() => this._safeFireAction('onSave', ...args));
+		},
+
+		cancel (...args) {
+			get(this, 'updateImageFileTask').cancelAll();
+			return this._safeFireAction('onCancel', ...args);
+		}
+	},
+
+	// Fire an action if we were given it, passing all the supplied arguments.
+	_safeFireAction (actionName, ...args) {
+		const action = get(this, actionName);
+		if (action)
+		{
+			return action(...args);
+		}
+	}
 });
